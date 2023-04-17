5cd453851fdcf1c5f749916fa7fb246b29e182ca
Ron Bachar <ron.bachar@outlook.com>
2023-04-17 15:17:42 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// if we can get the coords:
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
+}
