a33f19ab72d517679a14215dad19ff767656f217
Ron Bachar <ron.bachar@outlook.com>
2023-04-17 16:15:48 +0300

    google map

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-'use strict';
-
-// if we can get the coords:
-
 function myMap() {
   var mapProp = {
     center: new google.maps.LatLng(51.508742, -0.12085),
     zoom: 5,
   };
+
   var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
 }
